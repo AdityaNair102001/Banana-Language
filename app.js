e578dee389fa6c83e0e4762038cdcb59feb3e609
AdityaNair102001 <adityanair102001@gmail.com>
2020-12-18 23:00:35 +0530

    fetching api done

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 var btntranslate= document.querySelector("#btn-translate");
 var txtinput=document.querySelector("#txt-input");
+var txtoutput=document.querySelector("#output-area");
 
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
+
+function getFullURL(text)
+{
+    return serverUrl+"?"+"text="+text;
+}
+
+function errorHandler(error)
+{
+    console.log("error occured",error)
+}
 
 function show() {
    
-    console.log("button pressed");
-    console.log("input",txtinput.value);
+    var inputText=txtinput.value;
+
+    fetch(getFullURL(inputText))
+    .then(function (response) {
+            return response.json();
+        })
+    .then(json=>{
+        var Translatedtext=json.contents.translated;
+        txtoutput.innerText = Translatedtext;
+
+    });
+
 }
 
 btntranslate.addEventListener("click",show);
+
+
 
 
